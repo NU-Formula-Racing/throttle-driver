d347f1f649e8f7ff041092c6825f1f9ba0fe4416
jxu720 <126126593+jxu720@users.noreply.github.com>
2023-11-15 19:33:18 -0600

    throttle driver

--- include/throttle_driver.h
+++ include/throttle_driver.h
@@ -13,11 +13,23 @@
 // Build throttle class
 class Throttle {
     private:
-    ICAN &can_interface_;
+        ICAN &can_interface_;
+        const uint16_t kReceiveId = 0x000; // Replace with real address later
+
+        CANSignal<uint8_t, 0, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> throttle_percent{};
+        CANSignal<bool, 8, 1, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> brake_pressed{};
+        CANSignal<bool, 9, 1, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> throttle_active{};
+
+        CANRXMessage<3> throttle_message{can_interface_,
+                                         kReceiveId,
+                                         throttle_percent,
+                                         brake_pressed,
+                                         throttle_active};
 
     public:
-    Throttle(ICAN &can_interface_) : can_interface_(can_interface_){};
-    uint16_t GetThrottleAngle();
-    bool IsBrakePressed();
-    bool IsThrottleActive();
+        Throttle(ICAN &can_interface_) : can_interface_(can_interface_){};
+        void RXCallback();
+        uint16_t GetThrottlePercent();
+        bool IsBrakePressed();
+        bool IsThrottleActive();
 };
